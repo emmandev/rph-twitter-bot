280059c3badd5709f7d5742325c8e31f6e6c2201
fakers <warrenfakers@gmail.com>
2017-10-12 15:45:51 +0800

    apply beautify on codes index.js
    change readme title hahahaha

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ require('dotenv').config();
 
 const Snoowrap = require('snoowrap');
 const Snoostorm = require('snoostorm');
-const Twit = require('twit'); 
+const Twit = require('twit');
 const Reddit = require('./reddit');
-const Twitter  = require('./twitter');
+const Twitter = require('./twitter');
 
 const striptags = require('striptags');
 
@@ -25,26 +25,26 @@ const twitter = new Twitter();
 
 // Build Snoowrap and Snoostorm clients
 const r = new Snoowrap({
-    userAgent: 'reddit-bot-example-node',
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    username: process.env.REDDIT_USER,
-    password: process.env.REDDIT_PASS
+  userAgent: 'reddit-bot-example-node',
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  username: process.env.REDDIT_USER,
+  password: process.env.REDDIT_PASS
 });
 const client = new Snoostorm(r);
 
 const t = new Twit({
-    consumer_key: process.env.TWITTER_KEY,
-    consumer_secret: process.env.TWITTER_SECRET,
-    access_token: process.env.TWITTER_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_TOKEN_SECRET
+  consumer_key: process.env.TWITTER_KEY,
+  consumer_secret: process.env.TWITTER_SECRET,
+  access_token: process.env.TWITTER_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_TOKEN_SECRET
 });
 
 
 // Configure options for stream
 const streamOpts = {
-    subreddit: 'philippines',
-    results: 15
+  subreddit: 'philippines',
+  results: 15
 };
 
 
@@ -54,40 +54,38 @@ const comments = client.CommentStream(streamOpts);
 
 // On comment, see if user is asking to tweet ancestor content
 comments.on('comment', (comment) => {
-    /*
-     * Catches the phrase 'phlease tweet this!'
-     */
-    var regex = /phlease tweet this!\s*(\^{1}\d)?/gi,
-        match = regex.exec( comment.body );
+  /*
+   * Catches the phrase 'phlease tweet this!'
+   */
+  var regex = /phlease tweet this!\s*(\^{1}\d)?/gi,
+    match = regex.exec(comment.body);
 
-    if( match ){
-        // get parent comment
-        r.getComment(comment.parent_id)
-            .fetch()
-            .then((parentComment) => {
+  if (match) {
+    // get parent comment
+    r.getComment(comment.parent_id)
+      .fetch()
+      .then((parentComment) => {
 
-                // tweet it
-                t.post(
-                    'statuses/update',
-                    { 
-                        status: twitter.setContent({
-                            id:  comment.parent_id,
-                            body: striptags( parentComment.body_html ),
-                            user: parentComment.author.name,
-                            link: comment.link_permalink + parentComment.id
-                        }),
-                        enable_dm_commands: false
-                    },
-                    (error, data, response) => { 
-                        if( !error ){
-                            // Reply to user
-                            comment.reply( reddit.successTweetReply( data.id_str ) );
-                        }
-                        else {
-                            console.log(error);
-                        }
-                    }
-                );
-            });
-    }
+        // tweet it
+        t.post(
+          'statuses/update', {
+            status: twitter.setContent({
+              id: comment.parent_id,
+              body: striptags(parentComment.body_html),
+              user: parentComment.author.name,
+              link: comment.link_permalink + parentComment.id
+            }),
+            enable_dm_commands: false
+          },
+          (error, data, response) => {
+            if (!error) {
+              // Reply to user
+              comment.reply(reddit.successTweetReply(data.id_str));
+            } else {
+              console.log(error);
+            }
+          }
+        );
+      });
+  }
 });
